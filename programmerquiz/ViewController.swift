1628fab77914688faca15951d4a423af19b62f42
Aizhan-Khassenova <aizhan.kh@hotmail.com>
2023-05-06 17:33:12 +0600

    Initial Commit

---
+++ programmerquiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  programmerquiz
+//
+//  Created by Admin on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // hh
+    }
+
+
+}
+
