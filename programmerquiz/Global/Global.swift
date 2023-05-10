27122a02aaaeaa5c7f08bb490e94a8f2d7c9d0d5
Nurlan Ispayev <nurlanispaew@gmail.com>
2023-05-10 13:05:20 +0600

    Commit 5

---
+++ programmerquiz/Global/Global.swift
@@ -0,0 +1,21 @@
+//
+//  Global.swift
+//  programmerquiz
+//
+//  Created by nurlanispaew on 10.05.2023.
+//
+
+import UIKit
+
+var defaults = UserDefaults.standard
+
+class Global {
+    
+    // Firebase Path Database
+    static let fbPathGlobal     = "global" // False
+    static let fbPathAdMob      = "apps/english/eng/admob" // False
+    
+    //Yandex Metrika
+    static let metrikaID        = "6058a4cb-a6e2-4c94-bd46-7c2c9061ea38" // False
+    
+}
